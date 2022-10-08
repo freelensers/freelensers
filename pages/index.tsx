8932b9a50aa0023aa6614d018db4a8ea8e48b3bc
Esteban Zepeda <estzepeda@outlook.com>
2022-10-08 09:49:42 -0500

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -143,7 +143,7 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{''}
           <span>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
