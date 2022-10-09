f657983469107c05fc05f2ceb69b4893df83b3c2
InigoZepeda <inigis@hotmail.com>
2022-10-08 23:37:09 -0500

    Dynamic posts

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -30,9 +30,9 @@ const Navbar = ()=>{
                     <Link href='/Feed'>
                         Feed
                     </Link>
-                    <Link href='/Bounties'>
+                    {/* <Link href='/Bounties'>
                         Bounties
-                    </Link>
+                    </Link> */}
                     <li className="nav-item-bounties">
                         Create Bounties 
                     </li>
